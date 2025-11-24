3353a5ed4f070591d089c3325f4586c271ca4349
Iheb Zenkri <zenkriiheb@gmail.com>
2025-11-24 17:29:45 +0100

    initializing the parcel service + connecting to Neon ParcelDB

---
+++ parcel-service/src/main/java/com/smartagri/parcel/ParcelServiceApplication.java
@@ -0,0 +1,12 @@
+package com.smartagri.parcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelServiceApplication.class, args);
+	}
+
+}
